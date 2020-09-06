9614f3879681a5d2a4360bb33cb1f4f0d8f07931
abdulaziz-damlahi <abood_d@live.com>
2020-09-06 18:02:45 +0300

    Cards

--- lib/main.dart
+++ lib/main.dart
@@ -21,9 +21,73 @@ class app extends StatelessWidget {
               Text(
                 'abood damlahi',
                 style: TextStyle(
+                  fontFamily: 'LongCang-Regular',
                   fontSize: 40.0,
                   color: Colors.white,
                   fontWeight: FontWeight.bold,
+                ),
+              ),
+              Text(
+                'computer enginner',
+                style: TextStyle(
+                  fontFamily: 'PlayfairDisplay VariableFont wght',
+                  fontSize: 20.0,
+                  color: Colors.teal[100],
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              Card(
+                color: Colors.white,
+                margin: EdgeInsets.symmetric(vertical: 10, horizontal: 25),
+                child: Padding(
+                  padding: EdgeInsets.all(10.0),
+                  child: Row(
+                    children: <Widget>[
+                      Icon(
+                        Icons.phone,
+                        color: Colors.blue,
+                        size: 30,
+                      ),
+                      SizedBox(
+                        width: 10.0,
+                      ),
+                      Text(
+                        '+4555 555 555',
+                        style: TextStyle(
+                          color: Colors.blue,
+                          fontFamily: 'LongCang-Regular',
+                          fontSize: 20.0,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+              Card(
+                color: Colors.white,
+                margin: EdgeInsets.symmetric(vertical: 10, horizontal: 25),
+                child: Padding(
+                  padding: EdgeInsets.all(10.0),
+                  child: Row(
+                    children: <Widget>[
+                      Icon(
+                        Icons.email,
+                        color: Colors.blue,
+                        size: 30,
+                      ),
+                      SizedBox(
+                        width: 10.0,
+                      ),
+                      Text(
+                        'abood_d@live.com',
+                        style: TextStyle(
+                          color: Colors.blue,
+                          fontFamily: 'PlayfairDisplay VariableFont wght',
+                          fontSize: 20.0,
+                        ),
+                      ),
+                    ],
+                  ),
                 ),
               ),
             ],
